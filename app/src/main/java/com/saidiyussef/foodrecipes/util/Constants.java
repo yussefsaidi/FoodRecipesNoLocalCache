c512fbaa29cb2b9b5aba206a90404e9d1ec71290
Yussef <saidi.yussef@live.fr>
2020-01-29 17:39:21 -0500

    Add constants util class to store API

---
+++ app/src/main/java/com/saidiyussef/foodrecipes/util/Constants.java
@@ -0,0 +1,8 @@
+package com.saidiyussef.foodrecipes.util;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com/";
+    public static final String API_KEY = "";
+
+}
