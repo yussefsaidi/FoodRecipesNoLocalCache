a5639ddc8218f79dcb2b71eba5fbfa1a6ab876e0
Yussef <saidi.yussef@live.fr>
2020-01-31 20:53:08 -0500

    Add to constants time to wait on network response before timeout

--- app/src/main/java/com/saidiyussef/foodrecipes/util/Constants.java
+++ app/src/main/java/com/saidiyussef/foodrecipes/util/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 
     public static final String BASE_URL = "https://recipesapi.herokuapp.com/";
     public static final String API_KEY = "";
+    public static final int NETWORK_TIMEOUT = 3000;
 
 }
