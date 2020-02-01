fdf23f19866514c896d18d4aeb3d67c42a66d89c
Yussef <saidi.yussef@live.fr>
2020-01-31 20:54:43 -0500

    Add runnable class to be executed for API call

--- app/src/main/java/com/saidiyussef/foodrecipes/requests/RecipeApiClient.java
+++ app/src/main/java/com/saidiyussef/foodrecipes/requests/RecipeApiClient.java
@@ -1,17 +1,31 @@
 package com.saidiyussef.foodrecipes.requests;
 
+import android.os.Parcel;
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.saidiyussef.foodrecipes.AppExecutors;
 import com.saidiyussef.foodrecipes.models.Recipe;
+import com.saidiyussef.foodrecipes.requests.responses.RecipeSearchResponse;
+import com.saidiyussef.foodrecipes.util.Constants;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class RecipeApiClient {
 
     private MutableLiveData<List<Recipe>> mRecipes;
-
     private static RecipeApiClient instance;
+    private RetrieveRecipesRunnable mRetrieveRecipesRunnable;
+
 
     public static RecipeApiClient getInstance(){
         if(instance == null){
@@ -26,5 +40,77 @@ public class RecipeApiClient {
 
     public LiveData<List<Recipe>> getRecipes(){
         return mRecipes;
+    }
+
+    public void searchRecipesApi(String query, int pageNumber){
+        if(mRetrieveRecipesRunnable != null){
+            mRetrieveRecipesRunnable = null;
+        }
+        mRetrieveRecipesRunnable = new RetrieveRecipesRunnable(query, pageNumber);
+        final Future handler = AppExecutors.getInstance().networkIO().submit(mRetrieveRecipesRunnable);
+
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                // Let the user know its timed out
+                handler.cancel(true);
+            }
+        }, Constants.NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+
+    private class RetrieveRecipesRunnable implements Runnable{
+
+        private static final String TAG = "RetrieveRecipesRunnable";
+        private String query;
+        private int pageNumber;
+        boolean cancelRequest;
+
+        public RetrieveRecipesRunnable(String query, int pageNumber) {
+            this.query = query;
+            this.pageNumber = pageNumber;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+             try {
+                 Response response = getRecipes(query, pageNumber).execute();
+                if(cancelRequest == true){
+                    return;
+                }
+                if(response.code() == 200){
+                    List<Recipe> list = new ArrayList<>(((RecipeSearchResponse)response.body()).getRecipes());
+                    if(pageNumber == 1){
+                        mRecipes.postValue(list);
+                    } else{
+                        List<Recipe> currentRecipes = mRecipes.getValue();
+                        currentRecipes.addAll(list);
+                        mRecipes.postValue(currentRecipes);
+                    }
+                } else{
+                    String error = response.errorBody().string();
+                    Log.e(TAG, "run: " + error);
+                    mRecipes.postValue(null);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                mRecipes.postValue(null);
+            }
+
+
+
+        }
+
+        private Call<RecipeSearchResponse> getRecipes(String query, int pageNumber){
+            return ServiceGenerator.getRecipeApi().searchRecipe(
+                    query,
+                    String.valueOf(pageNumber)
+            );
+        }
+        
+        private void cancelRequest(){
+            Log.d(TAG, "cancelRequest: cancelling the search request.");
+            cancelRequest = true;
+        }
     }
 }
