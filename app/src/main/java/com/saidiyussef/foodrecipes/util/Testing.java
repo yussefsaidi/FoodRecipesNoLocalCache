8a19ef8137dd7499d3cc676db64ab6fc794dc614
Yussef <saidi.yussef@live.fr>
2020-02-01 20:48:34 -0500

    Add method for testing API

---
+++ app/src/main/java/com/saidiyussef/foodrecipes/util/Testing.java
@@ -0,0 +1,16 @@
+package com.saidiyussef.foodrecipes.util;
+
+import android.util.Log;
+
+import com.saidiyussef.foodrecipes.models.Recipe;
+
+import java.util.List;
+
+public class Testing {
+
+    public static void printRecipes(List<Recipe> list, String tag){
+        for(Recipe recipe : list){
+            Log.d(tag, "onChanged: " + recipe.getTitle());
+        }
+    }
+}
