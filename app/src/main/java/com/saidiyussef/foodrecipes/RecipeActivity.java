6720df3a8b86fdb190ac32e5177c34e2fa24afd2
Yussef <saidi.yussef@live.fr>
2020-02-18 23:08:11 -0500

    Search for recipe and setup observer

--- app/src/main/java/com/saidiyussef/foodrecipes/RecipeActivity.java
+++ app/src/main/java/com/saidiyussef/foodrecipes/RecipeActivity.java
@@ -8,13 +8,18 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.saidiyussef.foodrecipes.models.Recipe;
+import com.saidiyussef.foodrecipes.viewmodels.RecipeViewModel;
 
 public class RecipeActivity extends BaseActivity {
 
+    // Vars
+    private RecipeViewModel mRecipeViewModel;
     private static final String TAG = "RecipeActivity";
-    
+
     // UI components
     private ImageView mRecipeImage;
     private TextView mRecipeTitle, mRecipeRank;
@@ -31,14 +36,28 @@ public class RecipeActivity extends BaseActivity {
         mRecipeIngredientsContainer = findViewById(R.id.ingredients_container);
         mScrollView = findViewById(R.id.parent);
 
+        mRecipeViewModel = new ViewModelProvider(this).get(RecipeViewModel.class);
+
+        subscribeObservers();
         getIncomingIntent();
+    }
+
+    private void subscribeObservers(){
+        mRecipeViewModel.getRecipe().observe(this, new Observer<Recipe>() {
+            @Override
+            public void onChanged(Recipe recipe) {
+                if(recipe != null){
+                    Log.d(TAG, "onChanged: " + recipe.getTitle());
+                }
+            }
+        });
     }
 
 
     private void getIncomingIntent(){
         if(getIntent().hasExtra("recipe")){
             Recipe recipe = getIntent().getParcelableExtra("recipe");
-            Log.d(TAG, "getIncomingIntent: " + recipe.getTitle());
+            mRecipeViewModel.searchRecipeById(recipe.getRecipe_id());
         }
     }
 }
