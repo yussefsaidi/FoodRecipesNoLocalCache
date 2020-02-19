0938d1bf01070e1876abb53e51f8e64bf20b9b2f
Yussef <saidi.yussef@live.fr>
2020-02-18 23:23:16 -0500

    Update recipe layout after query

--- app/src/main/java/com/saidiyussef/foodrecipes/RecipeActivity.java
+++ app/src/main/java/com/saidiyussef/foodrecipes/RecipeActivity.java
@@ -2,6 +2,8 @@ package com.saidiyussef.foodrecipes;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -11,6 +13,8 @@ import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.saidiyussef.foodrecipes.models.Recipe;
 import com.saidiyussef.foodrecipes.viewmodels.RecipeViewModel;
 
@@ -37,7 +41,7 @@ public class RecipeActivity extends BaseActivity {
         mScrollView = findViewById(R.id.parent);
 
         mRecipeViewModel = new ViewModelProvider(this).get(RecipeViewModel.class);
-
+        showProgressBar(true);
         subscribeObservers();
         getIncomingIntent();
     }
@@ -47,7 +51,7 @@ public class RecipeActivity extends BaseActivity {
             @Override
             public void onChanged(Recipe recipe) {
                 if(recipe != null){
-                    Log.d(TAG, "onChanged: " + recipe.getTitle());
+                    setRecipeProperties(recipe);
                 }
             }
         });
@@ -59,5 +63,35 @@ public class RecipeActivity extends BaseActivity {
             Recipe recipe = getIntent().getParcelableExtra("recipe");
             mRecipeViewModel.searchRecipeById(recipe.getRecipe_id());
         }
+    }
+
+    private void setRecipeProperties(Recipe recipe){
+        if(recipe != null){
+            RequestOptions requestOptions = new RequestOptions()
+                    .placeholder(R.drawable.ic_launcher_background);
+            Glide.with(this)
+                    .setDefaultRequestOptions(requestOptions)
+                    .load(recipe.getImage_url())
+                    .into(mRecipeImage);
+            mRecipeTitle.setText(recipe.getTitle());
+            mRecipeRank.setText(String.valueOf(Math.round(recipe.getSocial_rank())));
+
+            mRecipeIngredientsContainer.removeAllViews();
+            for(String ingredient: recipe.getIngredients()){
+                TextView textView = new TextView(this);
+                textView.setText(ingredient);
+                textView.setTextSize(15);
+                textView.setLayoutParams(new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                ));
+                mRecipeIngredientsContainer.addView(textView);
+            }
+        }
+        showParent();
+        showProgressBar(false);
+    }
+
+    private void showParent(){
+        mScrollView.setVisibility(View.VISIBLE);
     }
 }
