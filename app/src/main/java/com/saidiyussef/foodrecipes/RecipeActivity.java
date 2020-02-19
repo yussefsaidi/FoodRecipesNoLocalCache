dd1e72b170058098e0ed31323e72a3a99aa2b2cd
Yussef <saidi.yussef@live.fr>
2020-02-18 21:48:12 -0500

    Add activity and layout for singular recipes

---
+++ app/src/main/java/com/saidiyussef/foodrecipes/RecipeActivity.java
@@ -0,0 +1,44 @@
+package com.saidiyussef.foodrecipes;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.saidiyussef.foodrecipes.models.Recipe;
+
+public class RecipeActivity extends BaseActivity {
+
+    private static final String TAG = "RecipeActivity";
+    
+    // UI components
+    private ImageView mRecipeImage;
+    private TextView mRecipeTitle, mRecipeRank;
+    private LinearLayout mRecipeIngredientsContainer;
+    private ScrollView mScrollView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+        mRecipeImage = findViewById(R.id.recipe_image);
+        mRecipeTitle = findViewById(R.id.recipe_title);
+        mRecipeRank = findViewById(R.id.recipe_social_score);
+        mRecipeIngredientsContainer = findViewById(R.id.ingredients_container);
+        mScrollView = findViewById(R.id.parent);
+
+        getIncomingIntent();
+    }
+
+
+    private void getIncomingIntent(){
+        if(getIntent().hasExtra("recipe")){
+            Recipe recipe = getIntent().getParcelableExtra("recipe");
+            Log.d(TAG, "getIncomingIntent: " + recipe.getTitle());
+        }
+    }
+}
